fc3977e32a97f929663082f225e3a57a73769448
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-10 15:16:03 +0900

    add handler

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"github.com/joho/godotenv"
+	"log"
+)
+
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := newRouter()
 
 	router.Logger.Fatal(router.Start(":8080"))
--- handler/handler.go
+++ handler/handler.go
@@ -1,10 +1,39 @@
 package handler
 
 import (
+	"context"
+	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/oauth2"
+	"log"
 	"net/http"
+	"os"
 )
 
 func Index(c echo.Context) error {
 	return c.JSON(http.StatusOK, "hello")
+}
+
+func Auth(c echo.Context) error {
+	ctx := context.Background()
+	issuer := "https//" + os.Getenv("AUTH0_DOMAIN") + "/"
+	provider, err := oidc.NewProvider(ctx, issuer)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := oauth2.Config{
+		ClientID: os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		RedirectURL: os.Getenv("/callback"),
+		Endpoint: provider.Endpoint(),
+		Scopes: []string{oidc.ScopeOpenID, "profile"},
+	}
+
+	state := "xyz" // 仮実装
+
+	authURL := config.AuthCodeURL(state)
+
+	return c.Redirect(http.StatusMovedPermanently, authURL)
 }
